723df3f43a15ed858f89c79f446274f20166bcf8
Alejandro-Moreira <moreiramikel11@gmail.com>
2024-06-09 22:42:56 -0500

    Hoja de Vida con firebase Hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDAT1zSZ-KrdUY0Tm83yyeDfPbzdq_wRNE",
+    authDomain: "hoja-de-vida-8e9b5.firebaseapp.com",
+    projectId: "hoja-de-vida-8e9b5",
+    storageBucket: "hoja-de-vida-8e9b5.appspot.com",
+    messagingSenderId: "1086126961610",
+    appId: "1:1086126961610:web:1bd64390c47d8652bb9533"
+  }
 };
