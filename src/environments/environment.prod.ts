8b951bf04350ee9ba6890e7916fe43b10365c426
Alejandro Moreira <117743484+Alejandro-Moreira@users.noreply.github.com>
2024-06-09 23:10:10 -0500

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebaseConfig : {
-    apiKey: "AIzaSyDAT1zSZ-KrdUY0Tm83yyeDfPbzdq_wRNE",
-    authDomain: "hoja-de-vida-8e9b5.firebaseapp.com",
-    projectId: "hoja-de-vida-8e9b5",
-    storageBucket: "hoja-de-vida-8e9b5.appspot.com",
-    messagingSenderId: "1086126961610",
-    appId: "1:1086126961610:web:1bd64390c47d8652bb9533"
-  }
+  production: true
 };
